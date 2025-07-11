0dae0bd10d57de013b7c74bb2cc1f73d022c5a63
Hakan Sarıdiken <hakansdiken@gmail.com>
2025-07-11 15:40:47 +0300

    added login page (html) for test auth

--- library-api/index.js
+++ library-api/index.js
@@ -4,16 +4,24 @@ import dotenv from 'dotenv'
 import bookController from './presentation/book.controller.js';
 import authController from './presentation/auth.controller.js';
 import { sessionMiddleware } from './infrastructure/middlewares/session.middleware.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(sessionMiddleware);
-app.use('/api/', authController);
+app.use('/api', authController);
+
 app.use('/api/books', bookController);
 
 const PORT = process.env.PORT;
