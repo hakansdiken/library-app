40ca440cd442366ef174c985eddae094a7b6f8be
Hakan Sarıdiken <hakansdiken@gmail.com>
2025-08-21 15:23:04 +0300

    Fix  user select dialog

--- library-ui/src/app/shared/components/user/user-select-dialog/user-select-dialog.component.ts
+++ library-ui/src/app/shared/components/user/user-select-dialog/user-select-dialog.component.ts
@@ -8,11 +8,26 @@ import { UserService } from '../../../../core/services/user/user.service';
 import { MatCardModule } from '@angular/material/card';
 import { CreateBorrow } from '../../../../core/models/borrow/create-borrow.model';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-user-select-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule, MatTableModule, MatCardModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './user-select-dialog.component.html',
   styleUrls: ['./user-select-dialog.component.css']
 })
@@ -24,6 +39,7 @@ export class UserSelectDialogComponent implements OnInit {
   pageIndex: number = 0;
   itemsPerPage: number = 10;
   totalItems?: number;
+  searchKey?: string = '';
 
   constructor(private userService: UserService, private dialogRef: MatDialogRef<UserSelectDialogComponent>) { }
 
@@ -34,15 +50,15 @@ export class UserSelectDialogComponent implements OnInit {
 
   loadUsers() {
 
-    this.userService.getAllUsers(this.pageIndex, this.itemsPerPage).subscribe({
+    this.userService.getAllUsers(this.pageIndex, this.itemsPerPage, this.searchKey).subscribe({
 
       next: (res) => {
 
         this.users = res.data ?? [];
 
-          this.pageIndex = Number(res.pagination?.pageIndex ?? 0);
-          this.totalItems = Number(res.pagination?.totalItems)
-          this.itemsPerPage = Number(res.pagination?.itemsPerPage ?? 10)
+        this.pageIndex = Number(res.pagination?.pageIndex ?? 0);
+        this.totalItems = Number(res.pagination?.totalItems)
+        this.itemsPerPage = Number(res.pagination?.itemsPerPage ?? 10)
       },
       error: (err) => {
         console.error("Error: ", err.error.message);
